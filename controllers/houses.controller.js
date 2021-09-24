dd6b4c9188d9b5aa0fb38cb7b65a409e8c4aa9f0
cromerohernandez <c.romero.hsv@gmail.com>
2021-09-24 13:44:24 +0200

    ADD: house.model & houses.controller

---
+++ controllers/houses.controller.js
@@ -0,0 +1,76 @@
+const createError = require('http-errors')
+
+const House = require('../models/house.model')
+
+module.exports.create = (req, res, next) => {
+  const { address, area, description, features, price } = req.body
+
+  const house = new House({
+    address: {
+      city: address.city,
+      door: address.door,
+      floor: address.floor,
+      street_type: address.street_type,
+      street_name: address.street_name,
+      street_number: address.street_number,
+    },
+    area: area,
+    description: description,
+    features: {
+      bathrooms: features.bathrooms,
+      bedrooms: features.bedrooms,
+      garden: features.garden,
+      terrace: features.terrace,
+      type: features.type,
+    },
+    price: price,
+  })
+
+  house.save()
+    .then(house => res.status(201).json(house))
+    .catch(next)
+}
+
+module.exports.get = (req, res, next) => {
+  House.findOne({ _id: req.params.id })
+    .then(house => {
+      if (!house) {
+        throw createError(404, 'house not found')
+      } else {
+        res.status(200).json(house)
+      }
+    })
+    .catch(next)
+}
+
+module.exports.update = (req, res, next) => {  
+  House.findOne({ _id: req.params.id })
+    .then(house => {
+      if (!house) {
+        throw createError(404, 'house not found')
+      } else {
+        ['address', 'area', 'description', 'features', 'price'].forEach(key => {
+          if (req.body[key]) {
+            house[key] = req.body[key]
+          }
+        })
+        house.save()
+          .then(updatedHouse => {
+            res.status(200).json(updatedHouse)
+          })
+      }
+    })
+    .catch(next)
+}
+
+module.exports.delete = (req, res ,next) => {
+  House.findByIdAndDelete(req.params.id)
+    .then(house => {
+      if (!house) {
+        throw createError(404, 'house not found')
+      } else {
+        res.status(204).json()
+      }
+    })
+    .catch(next)
+}
---
+++ models/house.model.js
@@ -0,0 +1,71 @@
+const mongoose = require('mongoose')
+
+const houseSchema = new.mongoose.Schema({
+  address: {
+    city: {
+      type: String,
+      required: [true, 'city is required'],
+    },
+    door: {
+      type: String,
+    },
+    floor: {
+      type: String,
+    },
+    street_type: {
+      type: String,
+      required: [true, 'road_type is required'],
+      enum: ['Avenida', 'Bulevar', 'Calle', 'Callejón', 'Camino', 'Pasaje', 'Plaza', 'Ronda', 'Travesía']
+    },
+    street_name: {
+      type: String,
+      required: [true, 'road_name is required'],
+    },
+    street_number: {
+      type: Number,
+      required: [true, 'road_number is required'],
+    }
+  },
+  area : {
+    type: Number
+  },
+  description: {
+    type: String
+  },
+  features: {
+    bathrooms: {
+      type: Number
+    },  
+    bedrooms: {
+      type: Number
+    },
+    garden: {
+      type: Boolean
+    },
+    terrace: {
+      type: Boolean
+    },
+    type: {
+      type: String,
+      enum: ['Ático', 'Bajo', 'Casa', 'Chalet independiente', 'Chalet adosado', 'Piso']
+    }
+  },
+  price: {
+    type: Number,
+    required: [true, 'price is required'],
+  }
+},
+{ timestamps: true,
+  toJSON: {
+    transform: (doc, ret) => {
+      ret.id = doc._id;
+      delete ret._id;
+      delete ret.__v;
+      return ret;
+    }
+  }
+})
+
+const house = mongoose.model('house', houseSchema)
+
+module.exports = house
