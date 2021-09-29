72620652ae01053d29e62b7d7be070943a517ec8
cromerohernandez <c.romero.hsv@gmail.com>
2021-09-29 18:12:44 +0200

    DEV: routes

--- config/routes.js
+++ config/routes.js
@@ -5,10 +5,10 @@ const housesController = require('../controllers/houses.controller')
 
 const cacheMiddleware = require('../middlewares/cache.middleware')
 
-router.post('/houses/new', cacheMiddleware.clearCache, housesController.create)
+router.post('/houses/new', cacheMiddleware.delAllKeysBasicsCache, housesController.create)
 router.get('/houses', cacheMiddleware.cache(1 * 60), housesController.getBasics)
 router.get('/houses/:id', cacheMiddleware.cache(1 * 60), housesController.getDetail)
 router.patch('/houses/:id', cacheMiddleware.delKeyDetailCache, cacheMiddleware.delAllKeysBasicsCache, housesController.update)
-router.delete('/houses/:id', cacheMiddleware.clearCache, housesController.delete)
+router.delete('/houses/:id', cacheMiddleware.delAllKeysBasicsCache, housesController.delete)
 
 module.exports = router
